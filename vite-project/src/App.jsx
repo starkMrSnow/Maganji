ab4ce03168fde5fffb6fed77df3390de3a6c8966
starkMrSnow <175dollarsnow@gmail.com>
2025-02-18 09:29:34 +0300

    set up pages

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Demo from './pages/demo/demo.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Routes>
+          <Route path="/demo" element={<Demo />} /> 
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+       
+     
+    </Router>
+  );
 }
-
-export default App
---
+++ vite-project/src/pages/Demo/demo.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Demo.css';
+import { FaBars } from "react-icons/fa";
+
+export default function Demo() { 
+  return (
+    <div>
+      <div>
+        header
+      </div>
+      <div>
+        
+        <FaBars />
+      </div>
+      <div>
+        footer
+      </div>
+    </div>
+  );
+}
