729de9b77f3a21f34e5dc5fc059ebb30fadba108
starkMrSnow <175dollarsnow@gmail.com>
2025-03-05 18:25:31 +0300

    layout

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/home.jsx';
+import Transaction from './pages/Transaction/transaction.jsx'
+import Layout from './pages/Layout/layout.jsx'
+import SpendingHabits from './pages/SpendingHabits/spendinghabits.jsx'
+import Goals from './pages/Goals/goals.jsx'
+import Profile from './pages/Profile/profile.jsx'
+import Settings from './pages/Settings/settings.jsx'
+// import Settings from './pages/Settings/settings.jsx'
+import Login from './pages/Login/login.jsx'
+import Signup from './pages/Signup/signup.jsx'
 
 export default function App() {
   return (
     <Router>
       <div>
       <Routes>
+      <Route path="/layout" element={<Layout />} /> 
           <Route path="/home" element={<Home />} /> 
+          <Route path="/spendinghabits" element={<SpendingHabits />} /> 
+          <Route path="/goals" element={<Goals />} /> 
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/settings" element={<Settings />} /> 
+          <Route path="/transaction" element={<Transaction />} /> 
         </Routes>
       </div>
        
