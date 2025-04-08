7715db3fe5d0c7345a6778f97567b902deb4dc97
cecillwala <lwalacecil13@gmail.com>
2025-04-08 11:55:24 +0300

    budget and goals table

--- backend/maganji/api/models.py
+++ backend/maganji/api/models.py
@@ -3,32 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    phone_no = models.CharField(max_length=15)
     national_id = models.IntegerField(unique=True)
     balance = models.FloatField(default=0.0)
 
-    # def generate_unique_username(self):
-    #     base_username = slugify(f"{self.first_name}{self.last_name}")
-    #     username = base_username
-    #     counter = 1
-
-    #     while User.objects.filter(username=username).exists():
-    #         username = f"{base_username}{counter}"
-    #         counter += 1
-        
-    #     return username
-
-    # def save(self, *args, **kwargs):
-    #     if not self.username:  # Only generate if username is not provided
-    #         self.username = self.generate_unique_username()
-    #     super().save(*args, **kwargs)
 
 
-# class Budget(models.Model):
-#     user_id = models.ForeignKey(User, on_delete=models.CASCADE, relation_name="person")
-#     amount = models.FloatField()
-#     due_date = models.DateTimeField(auto_now=True)
-#     budget = models.CharField(max_length=20)
+class Budget(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    amount = models.FloatField()
+    due_date = models.DateTimeField(auto_now=True)
+    budget = models.CharField(max_length=20)
 
 # class Transaction(models.Model):
 #     transaction_id = models.CharField()
@@ -38,9 +22,9 @@ class User(AbstractUser):
 #     date = models.DateTimeField()
 #     type = models.enums()
 
-# class Goal(models.Model):
-#     user_id = models.ForeignKey(User, on_delete=models.CASCADE, relation_name="person")
-#     amount = models.FloatField()
-#     duration = models.DateTimeField()
-#     status = models.BooleanField(default=False)
-#     goal = models.CharField(max_length=50)
+class Goal(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="person")
+    amount = models.FloatField()
+    maturity_date = models.DateTimeField()
+    status = models.BooleanField(default=False)
+    goal = models.CharField(max_length=50)
